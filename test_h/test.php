b8adb49fd85cc85f43edf0b426912deb1a11750c
Rafael Calleja <rafa.calleja@d-noise.net>
2016-07-05 15:08:49 +0200

    test h

---
+++ test_h/test.php
@@ -0,0 +1,107 @@
+<?php
+
+class iterable implements Countable, Iterator
+{
+
+    /**
+     * An array containing the entries
+     *
+     * @var array
+     */
+    private $elements;
+
+
+    public function __construct(array $elements = array())
+    {
+        $this->elements = $elements;
+    }
+
+    /**
+     * @return []
+     */
+    public function toArray()
+    {
+        return $this->elements;
+    }
+
+    /**
+     * Return the current element
+     * @link http://php.net/manual/en/iterator.current.php
+     * @return mixed Can return any type.
+     * @since 5.0.0
+     */
+    public function current()
+    {
+        return current($this->elements);
+    }
+
+    /**
+     * Move forward to next element
+     * @link http://php.net/manual/en/iterator.next.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function next()
+    {
+        next($this->elements);
+    }
+
+    /**
+     * Return the key of the current element
+     * @link http://php.net/manual/en/iterator.key.php
+     * @return mixed scalar on success, or null on failure.
+     * @since 5.0.0
+     */
+    public function key()
+    {
+        return key($this->elements);
+    }
+
+    /**
+     * Checks if current position is valid
+     * @link http://php.net/manual/en/iterator.valid.php
+     * @return boolean The return value will be casted to boolean and then evaluated.
+     * Returns true on success or false on failure.
+     * @since 5.0.0
+     */
+    public function valid()
+    {
+        return key($this->elements) !== null;
+    }
+
+    /**
+     * Rewind the Iterator to the first element
+     * @link http://php.net/manual/en/iterator.rewind.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function rewind()
+    {
+        reset($this->elements);
+    }
+
+    /**
+     * Count elements of an object
+     * @link http://php.net/manual/en/countable.count.php
+     * @return int The custom count as an integer.
+     * </p>
+     * <p>
+     * The return value is cast to an integer.
+     * @since 5.1.0
+     */
+    public function count()
+    {
+        return count($this->elements);
+    }
+}
+
+
+$elements = array('ele', 1, 2, 'foo', 'bar');
+$object = new iterable($elements);
+
+foreach($object as $key => $value)
+{
+   assert(in_array($value, $elements));
+}
+
+assert(count($object) == count($elements));
